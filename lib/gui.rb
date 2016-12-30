32e6ba5eca7f647449e6007848fbdf05f015f344
Hu Yifei <ascalon0823@gmail.com>
2016-12-30 11:02:36 +0800

    Add universal crud ui for non-primary classes

--- lib/gui.rb
+++ lib/gui.rb
@@ -16,16 +16,21 @@ class Gui < Gtk::Window
     window.add(table_main)
     b_attr = Gtk::Button.new(:label => "Attributes")
     b_attr.signal_connect "clicked" do |_widget|
-      AttributeWindow.new    
+      AttributeWindow.new   
     end
     table_main.attach(b_attr,0,1,0,1)
     
     b_field = Gtk::Button.new(:label => "Fields")
     b_field.signal_connect "clicked" do |_widget|
-      FieldWindow.new    
+      crudWindow("Field")   
     end
     table_main.attach(b_field,0,1,1,2)
     
+    b_agent = Gtk::Button.new(:label => "Agents")
+    b_agent.signal_connect "clicked" do |_widget|
+      crudWindow("Agent")
+    end
+    table_main.attach(b_agent,0,1,2,3)
     b_quit = Gtk::Button.new(:label=>"Quit")
     b_quit.signal_connect "clicked" do |_widget|
       Gtk.main_quit
@@ -35,6 +40,151 @@ class Gui < Gtk::Window
     window.show_all
     Gtk.main
   end
+  #CRUD UI of an object
+  def crudWindow(className)
+    window = Gtk::Window.new("className")
+    window.set_size_request(1366, 768)
+    window.set_border_width(1)
+
+    table = Gtk::Table.new(3,5,true)
+    window.add(table)
+    #Text Field#
+    textView = Gtk::TextView.new
+    table.attach(textView,0,5,0,2)
+
+    b_Load = Gtk::Button.new(:label => "Load %s" % className)
+    b_Load.signal_connect "clicked" do |_widget|
+      textView.buffer.text= "Loading...\n" + Engine.loadClass(className)    
+    end
+    table.attach(b_Load,0,1,2,3)
+
+    b_Save = Gtk::Button.new(:label => "Save %s" % className)
+    b_Save.signal_connect "clicked" do |_widget|
+      textView.buffer.text= "Saving...\n" + Engine.saveClass(className)
+    end
+    table.attach(b_Save,1,2,2,3)
+
+    b_Display = Gtk::Button.new(:label => "Display %s" % className)
+    b_Display.signal_connect "clicked" do |_widget|
+      textView.buffer.text= "Displaying...\n" + Utility.msg(Engine.dispClass(className))
+    end
+    table.attach(b_Display,2,3,2,3)
+
+    b_Add = Gtk::Button.new(:label => "Add new %s" % className)
+    b_Add.signal_connect "clicked" do |_widget|
+      addWindow(className)
+    end
+    table.attach(b_Add,3,4,2,3)
+
+    b_Del = Gtk::Button.new(:label => "Delete %s" % className)
+    b_Del.signal_connect "clicked" do |_widget|
+      deleteWindow(className)
+    end
+    table.attach(b_Del,4,5,2,3) 
+
+    window.signal_connect("delete-event") { |_widget| window.hide() }
+    window.show_all
+  end
+  #UI for adding an object
+  def addWindow(className)
+    num_attr = Engine.num_attr(className) + 1 #extra one for name
+    window = Gtk::Window.new("Add new %s" % className)
+    height = 100*(num_attr%10)+100
+    width = 300*(num_attr/10+1)
+    window.set_size_request(height,width)
+    window.set_border_width(1)
+
+    table_add = Gtk::Table.new(height/100,width/100,true)
+    window.add(table_add)
+
+    label_entry_name = Gtk::Label.new("%s Name" % className)
+    entry_name = Gtk::Entry.new
+    table_add.attach(entry_name,1,3,0,1)
+    table_add.attach(label_entry_name,0,1,0,1)
+    entries=[]
+    counter=0
+    Engine.attributes(className).each{|key,value|
+        counter+=1
+        label_entry_key = Gtk::Label.new(key)
+        entry_key = Gtk::Entry.new
+        table_add.attach(entry_key,counter/10*3+1,counter/10*3+3,counter%10,counter%10+1)
+        table_add.attach(label_entry_key,counter/10*3,counter/10*3+1,counter%10,counter%10+1)
+        entries<<entry_key
+    }
+
+    b_cancel = Gtk::Button.new(:label => "Cancel")
+    table_add.attach(b_cancel,0,1,height/100-1,height/100)
+    b_cancel.signal_connect "clicked" do |_widget|
+       window.hide()
+    end
+
+    b_add = Gtk::Button.new(:label => "Add")
+    table_add.attach(b_add,width/100-1,width/100,height/100-1,height/100)
+    args=[]
+    b_add.signal_connect "clicked" do |_widget|
+      name = entry_name.text
+      entries.each{|entry|
+        args<<entry.text
+      }
+
+      if(name==""||args.any? { |element| element ==""})
+        DialogWindow.new("Error","Entry cannot be empty")
+      else
+        obj = Engine.addObject(className,name, args)
+        DialogWindow.new("Add %s" % className,"%s has been added" % obj.detailText)
+        window.hide()
+      end
+    end
+    window.signal_connect("delete-event") { |_widget| window.hide() }
+    window.show_all
+  end
+  #UI for deleting an object
+  def deleteWindow(className)
+    window = Gtk::Window.new("Delete %s" % className)
+      window.set_size_request(400,300)
+      window.set_border_width(1)
+      table_del = Gtk::Table.new(2,3,true)
+      window.add(table_del)
+
+      Engine.loadClass(className)
+      objects = Engine.getList(className).keys
+      cbox_objects = Gtk::ComboBoxText.new
+      objects.each{ |obj|
+        cbox_objects.append_text obj
+      }
+      table_del.attach(cbox_objects, 0,1,0,1)
+
+      text_detail = Gtk::TextView.new
+      table_del.attach(text_detail,1,3,0,1)
+
+      b_cancel = Gtk::Button.new(:label => "Cancel")
+      b_cancel.signal_connect "clicked" do |_widget|
+        window.hide()
+      end
+      table_del.attach(b_cancel,0,1,1,2)
+
+      b_del = Gtk::Button.new(:label => "Delete")
+      b_del.signal_connect "clicked" do |_widget|
+        Engine.delObject(className,cbox_objects.active_iter[0])
+        window.hide()
+        DialogWindow.new("Delete %s" % className,"%s has been deleted" % [cbox_objects.active_iter[0]])
+      end
+      
+      table_del.attach(b_del,2,3,1,2)
+      b_del.sensitive=false
+      cbox_objects.signal_connect "changed" do 
+        if(cbox_objects.active_iter[0]!="")
+          b_del.sensitive =true
+          text_detail.buffer.text = Engine.find(className,cbox_objects.active_iter[0]).detailText
+        else
+          b_del.sensitive=false
+        end
+      end
+      window.signal_connect("delete-event") { |_widget| window.hide() }
+      window.show_all
+  end
+  
+  #Attribute is primary class, it use hard coded gui
   class AttributeWindow < Gtk::Window
     def initialize
       window = Gtk::Window.new("Attribute")
@@ -193,152 +343,6 @@ class Gui < Gtk::Window
         window.hide()
       end
 
-      window.signal_connect("delete-event") { |_widget| window.hide() }
-      window.show_all
-    end
-  end
-  class FieldWindow < Gtk::Window
-    def initialize
-      window = Gtk::Window.new("Field")
-      window.set_size_request(1366, 768)
-      window.set_border_width(1)
-
-      table_attr = Gtk::Table.new(3,5,true)
-      window.add(table_attr)
-      #Text Field#
-      textView = Gtk::TextView.new
-      table_attr.attach(textView,0,5,0,2)
-
-      b_fieldLoad = Gtk::Button.new(:label => "Load Fields")
-      b_fieldLoad.signal_connect "clicked" do |_widget|
-        textView.buffer.text= "Loading...\n" + Engine.loadField    
-      end
-      table_attr.attach(b_fieldLoad,0,1,2,3)
-
-      b_fieldSave = Gtk::Button.new(:label => "Save Fields")
-      b_fieldSave.signal_connect "clicked" do |_widget|
-        textView.buffer.text= "Saving...\n" + Engine.saveField
-      end
-      table_attr.attach(b_fieldSave,1,2,2,3)
-
-      b_fieldDisplay = Gtk::Button.new(:label => "Display Fields")
-      b_fieldDisplay.signal_connect "clicked" do |_widget|
-        textView.buffer.text= "Displaying...\n" + Utility.msg(Engine.dispFields)
-      end
-      table_attr.attach(b_fieldDisplay,2,3,2,3)
-
-      b_fieldAdd = Gtk::Button.new(:label => "Add Field ")
-      b_fieldAdd.signal_connect "clicked" do |_widget|
-        AddFieldWindow.new
-      end
-      table_attr.attach(b_fieldAdd,3,4,2,3)
-
-      b_fieldDel = Gtk::Button.new(:label => "Delete Field")
-      b_fieldDel.signal_connect "clicked" do |_widget|
-        DeleteFieldWindow.new
-      end
-      table_attr.attach(b_fieldDel,4,5,2,3) 
-
-      window.signal_connect("delete-event") { |_widget| window.hide }
-      window.show_all
-    end
-  end
-
-  class AddFieldWindow < Gtk::Window
-     def initialize
-      num_attr = Field.num_attr + 1 #extra one for name
-      window = Gtk::Window.new("Add new field")
-      height = 100*(num_attr%10)+100
-      width = 300*(num_attr/10+1)
-      window.set_size_request(height,width)
-      window.set_border_width(1)
-
-      table_field_add = Gtk::Table.new(height/100,width/100,true)
-      window.add(table_attr_add)
-
-      label_entry_name = Gtk::Label.new("Field Name")
-      entry_name = Gtk::Entry.new
-      table_field_add.attach(entry_name,1,3,0,1)
-      table_field_add.attach(label_entry_name,0,1,0,1)
-      entries=[]
-      counter=0
-      Field.attributes.each{|key,value|
-          counter+=1
-          label_entry_key = Gtk::Label.new(key)
-          entry_key = Gtk::Entry.new
-          table_field_add.attach(entry_key,counter/10*3+1,counter/10*3+3,counter%10,counter%10+1)
-          table_field_add.attach(label_entry_key,counter/10*3,counter/10*3+1,counter%10,counter%10+1)
-          entries<<entry_key
-      }
-
-      b_cancel = Gtk::Button.new(:label => "Cancel")
-      table_field_add.attach(b_cancel,0,1,height/100-1,height/100)
-      b_cancel.signal_connect "clicked" do |_widget|
-         window.hide()
-      end
-
-      b_add = Gtk::Button.new(:label => "Add")
-      table_field_add.attach(b_add,width/100-1,width/100,height/100-1,height/100)
-      args=[]
-      b_add.signal_connect "clicked" do |_widget|
-        name = entry_name.text
-        entries.each{|entry|
-          args<<entry.text
-        }
-
-        if(name==""||args.any? { |element| element ==""})
-          DialogWindow.new("Error","Entry cannot be empty")
-        else
-          Engine.add_Field(name, args)
-          window.hide()
-        end
-      end
-      window.signal_connect("delete-event") { |_widget| window.hide() }
-      window.show_all
-    end
-  end
-
-   class DeleteFieldWindow < Gtk::Window
-    def initialize
-      window = Gtk::Window.new("Delete Attribute")
-      window.set_size_request(400,300)
-      window.set_border_width(1)
-      table_field_del = Gtk::Table.new(2,3,true)
-      window.add(table_field_del)
-
-      Engine.loadField
-      fields = Field.getList.keys
-      cbox_field = Gtk::ComboBoxText.new
-      fields.each{ |field|
-        cbox_field.append_text field
-      }
-      table_field_del.attach(cbox_field, 0,1,0,1)
-
-      text_field_detail = Gtk::TextView.new
-      table_field_del.attach(text_field_detail,1,3,0,1)
-
-      b_cancel = Gtk::Button.new(:label => "Cancel")
-      b_cancel.signal_connect "clicked" do |_widget|
-        window.hide()
-      end
-      table_field_del.attach(b_cancel,0,1,1,2)
-
-      b_del = Gtk::Button.new(:label => "Delete")
-      b_del.signal_connect "clicked" do |_widget|
-        Engine.del_Field(cbox_field.active_iter[0])
-        window.hide()
-        DialogWindow.new("Delete Field","%s has been deleted" % [cbox_field.active_iter[0]])
-      end
-      table_field_del.attach(b_del,2,3,1,2)
-      b_del.sensitive=false
-      cbox_field.signal_connect "changed" do 
-        if(cbox_field.active_iter[0]!="")
-          b_del.sensitive =true
-          text_field_detail.buffer.text = Field.find(cbox_field.active_iter[0]).detailText
-        else
-          b_del.sensitive=false
-        end
-      end
       window.signal_connect("delete-event") { |_widget| window.hide() }
       window.show_all
     end
